d4e686fb2785aa77c4c765acc17d2f46e1776b1f
senshi111 <bane.nicovic@gmail.com>
2019-03-22 22:12:26 +0100

    fix broken image

--- src/app/news-card/news-card.stories.ts
+++ src/app/news-card/news-card.stories.ts
@@ -2,7 +2,7 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
 
-import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatFormFieldModule, MatAutocompleteModule, MatInputModule, MatBadgeModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatFormFieldModule, MatAutocompleteModule, MatInputModule, MatBadgeModule, } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout'
 
 
@@ -17,36 +17,37 @@ import { likeActions } from '../shared/like-button/like-button.stories'
 import { starActions } from '../shared/star-button/star-button.stories'
 import { commentActions } from '../shared/comment-button/comment-button.stories'
 import { NewsArticle } from '../model/news-article';
+import { NgxsModule } from '@ngxs/store';
 
 export const testNewsArticle: NewsArticle = {
-    id: "12345657890987654321",
-    sourceImage: "http://www.nytimes.com/services/mobile/img/android-newsreader-icon.png",
-    title: "Wait, How Did You Get Into College?",
-    subTitle: "The New York Times",
-    description: "How first-generation students learn about the myth of meritocracy.",
-    articleImage: "https://static01.nyt.com/images/2019/03/17/opinion/sunday/17capocrucet/17capocrucet-facebookJumbo.jpg",
-    articleURL: "https://www.nytimes.com/2019/03/16/opinion/sunday/college-admissions-merit.html",
+    id: '12345657890987654321',
+    sourceImage: 'http://www.nytimes.com/services/mobile/img/android-newsreader-icon.png',
+    title: 'Wait, How Did You Get Into College?',
+    subTitle: 'The New York Times',
+    description: 'How first-generation students learn about the myth of meritocracy.',
+    articleImage: 'https://static01.nyt.com/images/2019/03/17/opinion/sunday/17capocrucet/17capocrucet-facebookJumbo.jpg',
+    articleURL: 'https://www.nytimes.com/2019/03/16/opinion/sunday/college-admissions-merit.html',
     numLikes: 1,
     hasLiked: false,
-    comments: ["Comment One", "Comment Two", "Comment Three"],
+    comments: ['Comment One', 'Comment Two', 'Comment Three'],
     isStared: false
 };
 
 export const testNewsArticle_brokenImage: NewsArticle = Object.assign({}, testNewsArticle);
-testNewsArticle_brokenImage.articleImage = "http://junklink/junkimage.png";
+testNewsArticle_brokenImage.articleImage = '../../assets/errorImg/cantLoadImg.png';
 
 export const testNewsArticle_longTitle: NewsArticle = Object.assign({}, testNewsArticle);
-testNewsArticle_longTitle.title = "Wait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?";
+testNewsArticle_longTitle.title = 'Wait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?\nWait, How Did You Get Into College?';
 
 
 export const testNewsArticle_longDescription: NewsArticle = Object.assign({}, testNewsArticle);
-testNewsArticle_longDescription.description = "How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.";
+testNewsArticle_longDescription.description = 'How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.How first-generation students learn about the myth of meritocracy.';
 
 export const testNewsArticle_shortTitle: NewsArticle = Object.assign({}, testNewsArticle);
-testNewsArticle_shortTitle.title = "Wait";
+testNewsArticle_shortTitle.title = 'Wait';
 
 export const testNewsArticle_shortDescription: NewsArticle = Object.assign({}, testNewsArticle);
-testNewsArticle_shortDescription.description = "How";
+testNewsArticle_shortDescription.description = 'How';
 
 
 
@@ -75,7 +76,8 @@ storiesOf('Composite/News Card', module)
                 MatFormFieldModule,
                 MatAutocompleteModule,
                 MatBadgeModule,
-                FlexLayoutModule
+                FlexLayoutModule,
+                NgxsModule.forRoot()
             ],
         }),
     )
