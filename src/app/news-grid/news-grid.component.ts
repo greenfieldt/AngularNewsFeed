222f9a62f511e691f3afe4316a152d0f68786116
Timothy Greenfield <timg@increatesoftware.com>
2019-03-26 13:43:30 -0600

    suggestions on how to reuse news-cards

--- src/app/news-grid/news-grid.component.ts
+++ src/app/news-grid/news-grid.component.ts
@@ -2,35 +2,38 @@ import { Component, OnInit, Input, EventEmitter, Output, Pipe, PipeTransform } f
 import { NewsArticle } from '../model/news-article';
 import { Store } from '@ngxs/store';
 import { LikeArticle, StarArticle } from 'src/shared/state/news.actions';
+import { NewsCardComponent } from '../news-card/news-card.component';
 
-export enum NewsCardOrientation {
-  leftToRight = 1,
-  topToBottom
+export enum NewsGridLayout {
+    OneBigThreeToTheLeft,
+    OneBigTwoUnder
+    //etc
 }
 
-export const enum NewsCardSize {
-  extraBig = 1,
-  big,
-  small
-}
 
 @Component({
-  selector: 'app-news-grid',
-  templateUrl: './news-grid.component.html',
-  styleUrls: ['./news-grid.component.css']
+    selector: 'app-news-grid',
+    templateUrl: './news-grid.component.html',
+    styleUrls: ['./news-grid.component.css']
 })
 export class NewsGridComponent implements OnInit {
 
+
+    @Input newsArticles: NewsArticles[5];
+    @viewChildren(NewsCardComponent) newsCards[];
+
+    /*
+ 
   public NewsCardOrientationEunm = NewsCardOrientation;
     @Input() newsCardOrientation: NewsCardOrientation = NewsCardOrientation.topToBottom;
     @Input() newsCardSize: NewsCardSize = NewsCardSize.big;
-
+ 
     @Input() newsArticle: NewsArticle;
     @Output() onViewArticle: EventEmitter<any> = new EventEmitter();
     @Output() onStar: EventEmitter<any> = new EventEmitter();
     @Output() onLiked: EventEmitter<any> = new EventEmitter();
     @Output() onComment: EventEmitter<any> = new EventEmitter();
-
+*/
     constructor(private store: Store) {
 
 
@@ -40,32 +43,16 @@ export class NewsGridComponent implements OnInit {
         // console.log("NewsArticle:", this.newsArticle);
     }
 
-
-    _onViewArticle() {
-        this.onViewArticle.emit(this.newsArticle.url);
-    }
-
-    _onLikeArticle() {
-        this.store.dispatch(new LikeArticle(this.newsArticle));
-    }
-    _onStarArticle() {
-        this.store.dispatch(new StarArticle(this.newsArticle));
-    }
-
-}
-
-@Pipe({
-  name: 'longContent'
-})
-export class LongContentPipe implements PipeTransform {
-
-  transform(value: string, args: string): string {
-      // let limit = args.length > 0 ? parseInt(args[0], 10) : 10;
-      // let trail = args.length > 1 ? args[1] : '...';
-      const limit = args ? parseInt(args, 10) : 10;
-      const trail = '...';
-
-      return value.length > limit ? value.substring(0, limit) + trail : value;
-  }
-
+    /*
+        _onViewArticle() {
+            this.onViewArticle.emit(this.newsArticle.url);
+        }
+    
+        _onLikeArticle() {
+            this.store.dispatch(new LikeArticle(this.newsArticle));
+        }
+        _onStarArticle() {
+            this.store.dispatch(new StarArticle(this.newsArticle));
+        }
+    */
 }
